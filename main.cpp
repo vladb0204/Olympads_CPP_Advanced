d748e6c6be8e2df7479a38372f86e76d965bcd3e
vladb0204 <vladbalandin2013@gmail.com>
2021-02-06 20:46:29 +0300

    Just updated a little bit

--- main.cpp
+++ main.cpp
@@ -1,17 +1,5 @@
 #include "read_olymp.h"
 
-////=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-#define ll int64_t
-#define ld long double
-#define all(x) begin(x), end(x)
-#define end_line '\n'
-#define space ' '
-
-using namespace std;
-
-////=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
 void solve() {
     return;
 }
